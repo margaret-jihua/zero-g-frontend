7a0c010318f810419c8ba892d9ab0b23aa8b96ff
jacob kleiman <jacobakleiman@gmail.com>
2020-10-14 15:55:14 -0700

    adds components

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti enim magnam eligendi blanditiis quidem repellendus soluta consequatur earum iste consectetur voluptatibus ut, in numquam quos modi dolorum. Reiciendis, maiores hic!</p>
+        </div>
+    )
+}
+
+export default About
