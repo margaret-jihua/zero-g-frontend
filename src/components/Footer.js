1226a654e6ea8382e5f07ba0c094bda7beb1b796
margare-jihua <margaret.jihua@gmail.com>
2020-10-14 18:59:52 -0700

    [Footer] update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer bg-dark">
             <div className="container text-center">
-                <span className="text-muted">@2020 Built with MERN </span>
+                <span className="text-muted">copyright &#169;2020 Zero-G, All Rights Reserved. </span>
             </div>
         </footer>
     )
