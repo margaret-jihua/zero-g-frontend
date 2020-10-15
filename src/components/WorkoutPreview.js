d031631f59a0b1b746f9cc01d407037e8e86f4dc
jacob kleiman <jacobakleiman@gmail.com>
2020-10-15 15:03:43 -0700

    adds link to workout preview

--- src/components/WorkoutPreview.js
+++ src/components/WorkoutPreview.js
@@ -1,12 +1,14 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const WorkoutPreview = ({ exercise }) => {
   return (
     <div>
       <h3>{exercise.name}</h3>
       <p>{exercise.description}</p>
-      <button>Start Workout</button>
+      <Link to={`workout/${exercise._id}`}>
+        <button>Start Workout</button>
+      </Link>
     </div>
   );
 };
