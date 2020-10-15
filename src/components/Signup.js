be85b6c5264a2bf1d42f94d1ecefae13469cad20
jacob kleiman <jacobakleiman@gmail.com>
2020-10-14 22:54:17 -0700

    dynamic form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,11 +6,29 @@ import Form from './Form';
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const Signup = () => {
+  /**
+   * state = {
+   *   name: '',
+   *  password: ''
+   * }
+   */
+
+  //  this.setState({
+  //          name: e.target.value
+  // })
+  // setState(e.target.value)
+  //   Form Fields
   let [name, setName] = useState('');
   let [email, setEmail] = useState('');
   let [password, setPassword] = useState('');
   let [confirmPassword, setConfirmPassword] = useState('');
+  let [mass, setMass] = useState('');
+  let [height, setHeight] = useState('');
+  let [age, setAge] = useState('');
+  let [boneDensity, setBoneDensity] = useState('');
+
   let [redirect, setRedirct] = useState('');
+  let [formIdx, setFormIdx] = useState(0);
 
   const handleName = (e) => {
     setName(e.target.value);
@@ -26,6 +44,25 @@ const Signup = () => {
 
   const handleConfirmPassword = (e) => {
     setConfirmPassword(e.target.value);
+  };
+
+  const handleAge = (e) => {
+    setAge(e.target.value);
+  };
+
+  const handleBoneDensity = (e) => {
+    setBoneDensity(e.target.value);
+  };
+  //   const fieldHandler = (e, setState) => {
+  //       setState(e.target.value)
+  //   }
+
+  const handleMass = (e) => {
+    setMass(e.target.value);
+  };
+
+  const handleHeight = (e) => {
+    setHeight(e.target.value);
   };
 
   const formData = [
@@ -57,23 +94,55 @@ const Signup = () => {
         },
       ],
     },
+    {
+      inputs: [
+        {
+          name: 'Mass',
+          value: mass,
+          onChange: handleMass,
+          type: 'range',
+        },
+        {
+          name: 'Height',
+          value: height,
+          onChange: handleHeight,
+          type: 'range',
+        },
+        {
+          name: 'Age',
+          value: age,
+          onChange: handleAge,
+          type: 'range',
+        },
+        {
+          name: 'Bone Density',
+          value: boneDensity,
+          onChange: handleBoneDensity,
+          type: 'range',
+        },
+      ],
+    },
   ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (password === confirmPassword) {
-      const newUser = { name, email, password, confirmPassword };
+    if (formIdx >= 1) {
+      if (password === confirmPassword) {
+        const newUser = { name, email, password, confirmPassword };
 
-      axios
-        .post(`${REACT_APP_SERVER_URL}/api/users/register`, newUser)
-        .then((response) => {
-          console.log(response);
-          setRedirct(true);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+        axios
+          .post(`${REACT_APP_SERVER_URL}/api/users/register`, newUser)
+          .then((response) => {
+            console.log(response);
+            setRedirct(true);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    } else {
+      setFormIdx(formIdx + 1);
     }
   };
 
@@ -84,7 +153,7 @@ const Signup = () => {
       <div className='col-md-7 offset-md-3'>
         <div className='card card-body'>
           <h2 className='py-2'>Signup</h2>
-          <Form inputs={formData[0].inputs} onSubmit={handleSubmit} />
+          <Form inputs={formData[formIdx].inputs} onSubmit={handleSubmit} />
         </div>
       </div>
     </div>
