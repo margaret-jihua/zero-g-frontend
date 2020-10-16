c50c7460c49bfdc29ffb745c4b4a6ba6ac56f17a
SUBRATA ROY <subrata.r321@gmail.com>
2020-10-15 22:02:11 -0400

    add profile datas

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,22 +2,53 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import WorkoutPreview from './WorkoutPreview';
 
+import plannet1 from '../imgs/Group 239.png'
+import plannet1_cir from '../imgs/Ellipse 78.png'
+import plannet1_cir2 from '../imgs/Ellipse 79.png'
+import circle from '../imgs/Card/Vector.png'
+
 const Profile = (props) => {
   const userData = props.user ? (
-    <div>
+    <div className="profileDiv">
       <h1>Hello {props.user.name}</h1>
-      <p>
-        You're on <strong>DAY 1</strong> on your trip to <span>Saturn</span>!
-      </p>
+      <div >
+        <img src={plannet1}/>
+        {/* <img src={plannet1_cir} style={{position: 'absolute', zIndex: '2'}}/>
+        <img src={plannet1_cir2} style={{position: 'absolute', zIndex: '3'}}/> */}
+      </div>
+      <div style={{position: 'relative'}}>
+        <p> 57% Complete</p>
+        <p>
+          You're on <strong>DAY 1</strong> on your trip to <span style={{ color: 'linear-gradient(#EB0000, #FFDB80)'}}>Saturn</span>!
+        </p>
 
-      {props.user.exercises ? (
+        <h3>Log your progress below</h3>
+        <p><button className="btn">BMD</button><span style={{marginLeft: '30px'}}></span><button className="btn">MASS</button></p>
+        <h3>Your Next Workout</h3>
+        <div className="workoutStatus">
+            <div>
+              <h4>Astronaut Run</h4>
+              <p> A hour intense run on the trademill perfect to achieve cardio goals.</p>
+            </div>
+              <div>
+                  <img src={circle} />
+              </div>
+              <a>Start Workout</a>
+              <p>
+                 60% Completed
+              </p>
+
+        </div>
+      </div>
+
+      {/* {props.user.exercises ? (
         <>
           <h3>Your Next Workout</h3>
           <WorkoutPreview exercise={props.user.exercises[0]} />
         </>
       ) : (
         ''
-      )}
+      )} */}
       {/* <p>
         <strong>Name: </strong>
         {props.user.name}
