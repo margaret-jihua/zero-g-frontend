6a8fe1953e0b034e3f3488537075a7996c3b5a6e
jacob kleiman <jacobakleiman@gmail.com>
2020-10-15 19:01:30 -0700

    adds new timer background

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,13 @@
 // Images
 import ActivityBackground from './activity-bg.png';
 import WorkoutBackground from './workouts-bg.png';
+import TimerBackground from './timer-bg.png';
+
 // Icons
 export * from './activity-button';
 
 export const Images = {
   ActivityBackground,
   WorkoutBackground,
+  TimerBackground,
 };
