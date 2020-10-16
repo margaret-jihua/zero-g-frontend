930d92dc0f3b6d33eb5d8cf0f5403ee61f5d6c6a
jacob kleiman <jacobakleiman@gmail.com>
2020-10-15 21:40:04 -0700

    adds timer styles

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
+/** @jsx jsx */
+
+import { css, jsx } from '@emotion/core';
+import styled from '@emotion/styled';
+import { useState, useEffect } from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
+const TimerButton = styled.button`
+  background-color: ${(props) => props.color};
+  width: 131px;
+  height: 131px;
+  border-radius: 50%;
+  transition: background-color 0.5s ease;
+  border: none;
+`;
+
 const Timer = (props) => {
+  const [buttonColorStates] = useState(['#00BA88', '#F15025', '#FFA3FD']);
+
   let defaultTime = props.defaultTime || 10;
   const [seconds, setSeconds] = useState(defaultTime);
   const [isActive, setIsActive] = useState(false);
@@ -32,25 +47,44 @@ const Timer = (props) => {
   }, [isActive, seconds]);
 
   return (
-    <div className='d-flex align-items-center flex-column'>
+    <div
+      css={css`
+        display: flex;
+        justify-content: space-around;
+        flex-direction: column;
+        align-items: center;
+        height: 70vh;
+      `}
+    >
       {/* <div className='time'>{seconds}s</div>
        */}
       <CircularProgressbar
+        styles={buildStyles({
+          textColor: 'white',
+          pathColor: '#FD58FF',
+          //   trailColor: 'gold',
+        })}
         value={(1 - seconds / defaultTime) * 100}
         text={seconds > 0 ? `${seconds}s` : `Done!`}
       />
       <div className='row'>
         {done ? (
-          <button onClick={() => console.log('Redirect')}>Next Workout!</button>
+          <TimerButton
+            color={buttonColorStates[2]}
+            onClick={() => console.log('Redirect')}
+          >
+            Return to Home
+          </TimerButton>
         ) : (
-          <button
+          <TimerButton
+            color={isActive ? buttonColorStates[1] : buttonColorStates[0]}
             className={`button button-primary button-primary-${
               isActive ? 'active' : 'inactive'
             }`}
             onClick={toggle}
           >
-            {isActive ? 'Pause' : 'Start'}
-          </button>
+            {isActive ? 'End Workout' : 'Start Workout'}
+          </TimerButton>
         )}
       </div>
     </div>
--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,4 +1,9 @@
-import React, { useState, useEffect } from 'react';
+/** @jsx jsx */
+
+import { css, jsx } from '@emotion/core';
+import styled from '@emotion/styled';
+import { useState, useEffect } from 'react';
+import { Images } from '../assets';
 import Timer from './Timer';
 import Form from './Form';
 
@@ -8,64 +13,102 @@ const Workout = (props) => {
   //     props.user.exercises &&
   //     props.user.exercises.find((el) => el._id === workoutID);
 
-  const [update, setUpdate] = useState(false);
+  //   const [update, setUpdate] = useState(false);
   const [workoutName, setWorkoutName] = useState('Workout Name');
-  const [workoutTime, setWorkoutTime] = useState(10);
+  //   const [workoutTime, setWorkoutTime] = useState(10);
 
-  const handleWorkoutName = (e) => {
-    setWorkoutName(e.target.value);
-  };
-  const handleWorkoutTime = (e) => {
-    setWorkoutTime(e.target.value);
-  };
+  //   const handleWorkoutName = (e) => {
+  //     setWorkoutName(e.target.value);
+  //   };
+  //   const handleWorkoutTime = (e) => {
+  //     setWorkoutTime(e.target.value);
+  //   };
 
-  const handleUpdate = () => {
-    //   PUT REQUEST
-    console.log('updated');
-    setUpdate(false);
-  };
+  //   const handleUpdate = () => {
+  //     //   PUT REQUEST
+  //     console.log('updated');
+  //     setUpdate(false);
+  //   };
 
-  const formInputs = {
-    inputs: [
-      {
-        name: 'Workout Name',
-        value: workoutName,
-        onChange: handleWorkoutName,
-        type: 'text',
-      },
-      {
-        name: 'Time (seconds)',
-        value: workoutTime,
-        onChange: handleWorkoutTime,
-        type: 'number',
-      },
-    ],
-  };
+  //   const formInputs = {
+  //     inputs: [
+  //       {
+  //         name: 'Workout Name',
+  //         value: workoutName,
+  //         onChange: handleWorkoutName,
+  //         type: 'text',
+  //       },
+  //       {
+  //         name: 'Time (seconds)',
+  //         value: workoutTime,
+  //         onChange: handleWorkoutTime,
+  //         type: 'number',
+  //       },
+  //     ],
+  //   };
 
   useEffect(() => {
-    if (props.user) {
-      const workoutID = window.location.pathname.split('/')[2];
-      const workout = props.user.exercises.find((el) => el._id === workoutID);
-      //   console.log(workout);
-      setWorkoutName(workout.name);
-      setWorkoutTime(parseInt(workout.time));
+    // if (props.user) {
+    const workout = window.location.pathname.split('/')[2];
+    switch (workout) {
+      case 'run':
+        setWorkoutName('Astronaut Run');
+        break;
+      case 'row':
+        setWorkoutName('Space Row');
+        break;
+      case 'yoga':
+        setWorkoutName('Space Yoga');
+        break;
+      case 'bike':
+        setWorkoutName('Meteor Bike');
+        break;
+      default:
+        setWorkoutName('Galaxy Squat');
+        break;
     }
-  }, [props]);
+    //   const workout = props.user.exercises.find((el) => el._id === workoutID);
+    //   console.log(workout);
+    // setWorkoutName(workout);
+    //   setWorkoutTime(parseInt(workout.time));
+    // }
+  }, []);
 
   return (
-    <div>
-      <h1>{workoutName}</h1>
-      {update ? (
-        <>
-          <h3>Too easy or hard? Update your workout!</h3>
-          <Form inputs={formInputs.inputs} onSubmit={handleUpdate} />
-        </>
-      ) : (
-        <>
-          <Timer defaultTime={workoutTime} />
-          <button onClick={() => setUpdate(true)}>Update Workout</button>
-        </>
-      )}
+    // <div>
+    //   <h1>{workoutName}</h1>
+    //   {update ? (
+    //     <>
+    //       <h3>Too easy or hard? Update your workout!</h3>
+    //       {/* <Form inputs={formInputs.inputs} onSubmit={handleUpdate} /> */}
+    //     </>
+    //   ) : (
+    //     <>
+    //       <Timer defaultTime={workoutTime} />
+    //       <button onClick={() => setUpdate(true)}>Update Workout</button>
+    //     </>
+    //   )}
+    // </div>
+    <div
+      css={css`
+        background-image: url('${Images.TimerBackground}');
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        // padding-top: 20px;
+        padding-top: 50px;
+      `}
+    >
+      <h1
+        css={css`
+          padding-top: 25px;
+        `}
+      >
+        {workoutName}
+      </h1>
+      <Timer defaultTime={60} />
     </div>
   );
 };
